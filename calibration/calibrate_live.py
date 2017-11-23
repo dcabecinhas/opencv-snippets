98e9fe4cf55833fc01e5e6e25ff8c88babac38da
David Cabecinhas <david.cab@gmail.com>
2017-11-24 00:23:17 +0800

    Calibrate live from camera without redundant frames

--- calibration/calibrate_live.py
+++ calibration/calibrate_live.py
@@ -7,6 +7,8 @@ import yaml
 import json
 from datetime import datetime
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 cap = cv2.VideoCapture(0)
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH,640);
 # cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480);
@@ -24,34 +26,63 @@ pattern_points *= square_size
 obj_points = []
 img_points = []
 
+n_images = 0
+n_continuous = 0
 while (True):
     # Capture frame-by-frame
     ret, image = cap.read()
 
-    found, corners = cv2.findChessboardCorners(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), pattern_size, cv2.CALIB_CB_FAST_CHECK)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    found, corners = cv2.findChessboardCorners(gray, pattern_size, cv2.CALIB_CB_FAST_CHECK)
 
     if found == True:
-        # Refine square detection
-        term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
-        cv2.cornerSubPix(image, corners, (5, 5), (-1, -1), term)
+        # Since we are using a camera at 30 fps 
+        # we use only every other 10 frames
+        if n_continuous % 10 == 0:
 
-        img_points.append(corners.reshape(-1, 2))
-        obj_points.append(pattern_points)
+            # Refine square detection
+            term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
+            cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), term)
 
-        # Save image to disc
-        filename = datetime.now().strftime('%Y%m%d_%Hh%Mm%Ss%f') + '.jpg'
-        cv2.imwrite(filename, image)
+            img_points.append(corners.reshape(-1, 2))
+            obj_points.append(pattern_points)
+
+            # # Save image to disc
+            # filename = datetime.now().strftime('%Y%m%d_%Hh%Mm%Ss%f') + '.jpg'
+            # cv2.imwrite(filename, image)
+
+            n_images += 1
 
         # Draw grid overlay
         cv2.drawChessboardCorners(image, pattern_size, corners, found)
 
+        n_continuous += 1
+
+    else:
+        n_continuous = 0
+
+    display_img = cv2.flip(image,1)
+    cv2.putText(display_img, "#{}".format(n_images), (0, 60), font, 3, (0, 0, 255), 3, cv2.LINE_AA)
+
     # display image (with grid overlay, if cound corners)
-    cv2.imshow('grid', cv2.flip(image,1))
+    cv2.imshow('grid', display_img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# When everything done, release the capture
+cap.release()
+
+cv2.destroyAllWindows()
+
+# Hack not to "hang" the window in *nix systems (Linux,Mac)
+cv2.waitKey(1)
+
+
+
 print("Calculating camera distortion...")
+
+h, w = image.shape[:2]
 
 # calculate camera distortion
 rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, (w, h), None, None)
@@ -67,9 +98,9 @@ print("distortion coefficients: ", dist_coefs.ravel())
 # It's very important to transform the matrix to list.
 data = {'camera_matrix': np.asarray(camera_matrix).tolist(),
         'dist_coeff': np.asarray(dist_coefs).tolist()}
-        
-with open("calibration.yaml", "w") as f:
-    yaml.dump(data, f)
+
+with open("calibration.json", "w") as f:
+    json.dump(data, f)
 with open("calibration.yaml", "w") as f:
     yaml.dump(data, f)
 
@@ -79,13 +110,4 @@ with open("calibration.yaml", "w") as f:
 #     loadeddict = yaml.load(f)
 #     camera_matrix_loaded = loadeddict.get('camera_matrix')
 #     dist_coefs_loaded = loadeddict.get('dist_coeff')
-
-
-# When everything done, release the capture
-cap.release()
-
-cv2.destroyAllWindows()
-
-# Hack not to "hang" the window in *nix systems (Linux,Mac)
-cv2.waitKey(1)
 
